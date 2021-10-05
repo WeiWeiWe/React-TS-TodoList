502a7e4c368e18438e9affe51d462f592773b3c7
Wei <aszx70293@gmail.com>
2021-10-05 09:37:39 +0800

    Add styled-reset & styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
